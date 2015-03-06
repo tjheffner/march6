52588f90273dd4f5c36d7f510ddfa500fc075d75
tanner heffner <tanner@tanners-MacBook-Pro.local>
2015-03-06 09:45:35 -0800

    Add add_contact page

--- assessment/app/app.php
+++ assessment/app/app.php
@@ -14,14 +14,28 @@ $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array ('twig.path' => __DIR__.'/../views'));
 
 //this page should display all existing contacts + a form for new ones + a button to clear all contacts
-$app->get("/", function() {
-  return "home";
+$app->get("/", function() use($app) {
+  return $app['twig']->render('main.twig', array('contacts' => Contact::getAll()));
 });
 
 //this page should show the newly created contact
+$app->post("/add_contact", function() use ($app) {
+  $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+  $contact->save();
+  return $app['twig']->render('add_contact.twig', array('newcontact' => $contact));
+});
 
 //this page should show the delete confirmation
 
 return $app;
 
 ?>
+
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
